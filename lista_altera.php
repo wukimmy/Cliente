8f7632b1a3a6014315578cedd878c67c9d960bb1
Kimmy Wu <wu_kimmy@outlok.com>
2018-10-31 00:08:30 -0300

    add edit php files

--- lista_altera.php
+++ lista_altera.php
@@ -10,10 +10,34 @@
     </head>
     <body>
         <div class="topnav">
-            <a href="index.html">Cadastro</a>
+            <a href="index.php">Cadastro</a>
             <a class="active" href="./lista_altera">Editar</a>
             <a href="./listagem">Listar</a>
             <a href="./lista_excluir">Excluir</a>
         </div>
     </body>
+    <div class="container">
+			<table> 
+				<h2>Escolha para fazer a exclusão</h2>
+				<ul>
+				<?php
+					include "conexao.php";
+					$consulta = "select * from cliente";
+					$resultado = mysqli_query($GLOBALS["conexao"],$consulta);
+					if($resultado != null)
+					{
+						while($registro = mysqli_fetch_object($resultado))
+						{
+							echo '<li>';
+							echo "<a href='formulario_alterar.php?cod=$registro->codigo'>";
+							echo $registro->nome ;
+							echo "</a><br>";
+							echo '</li>';
+						}
+					}
+				?>
+				</ul>
+			</table>															
+		
+		</div>
 </html>
