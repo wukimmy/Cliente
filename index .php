593b94f74234ef8a4cf60fc9e5ca0de4c173ca7f
Kimmy <wukimmy>
2018-11-13 06:49:52 -0200

    inital commit

---
+++ index .php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Clientes</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/index.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/nav.css" />
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="index.html">Cadastro</a>
+  <a href="#news">Editar</a>
+  <a href="#contact">Listar</a>
+  <a href="#about">Excluir</a>
+</div>
+<form id="contact">
+  <div class="container">
+    <div class="head">
+      <h2>Cadastro</h2>
+    </div>
+    <input type="text" name="codigo" placeholder="Código" /><br />
+    <input  type="text" name="nome" placeholder="Nome" /><br />
+    <input  type="text" name="endereco" placeholder="Endereço" /><br />
+    <button id="submit" type="submit">
+      Enviar
+    </button>
+  </div>
+</form>
+</body>
+</html>
